c108c2ebb0d67e4005b2514e34e38f0a01cb2ac4
504703038 <504703038@github.com>
2019-04-12 17:52:10 +0000

    updata

---
+++ study/Python/实验/实验5/习题5.5.py
@@ -0,0 +1,29 @@
+def isPrime(n):
+    if n == 2:
+        return True
+    if n <= 1:
+        return False
+    f = 1
+    for i in range(2, n):
+        if i * i >= n:
+            break
+        if n % i == 0:
+            f = 0
+            break
+    if f == 1:
+        return True
+    else:
+        return False
+
+
+while True:
+    try:
+        n = eval(input("请输入一个整数："))
+        ans = isPrime(n)
+    except NameError:
+        print("输入必须是自然数!请重新输入")
+    except TypeError:
+        print("输入必须是自然数!请重新输入")
+    else:
+        print(ans)
+        break
