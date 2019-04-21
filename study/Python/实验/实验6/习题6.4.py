6136d1b28bc0a4379aefe3067a0540e0828c56e1
504703038 <504703038@github.com>
2019-04-21 18:22:28 +0000

    updata

--- study/Python/实验/实验6/习题6.4.py
+++ study/Python/实验/实验6/习题6.4.py
@@ -1,5 +1,16 @@
 # 习题6.4
-import jieba
+# import jieba
 text = input("请输入一段文本字符：")
-words = jieba.cut(text)
-print(words)
+lab = list('!"#$%&()*+,-./:;<=>?@[\\]^_‘{|}~\' ，。、；“”')
+words = list(text)
+count = {}
+for word in words:
+    word = word.lower()
+    if word in lab:
+        continue
+    count[word] = count.get(word, 0) + 1
+ls = list(count.items())
+ls.sort(key=lambda x: x[1], reverse=True)
+# print(ls)
+for i in ls:
+    print(i[0])
