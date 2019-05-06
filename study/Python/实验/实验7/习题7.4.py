b45e9b15b42b438d51d8cf876d995ebaedb9f3bd
504703038 <504703038@github.com>
2019-05-06 22:27:29 +0000

    update

--- study/Python/实验/实验7/习题7.4.py
+++ study/Python/实验/实验7/习题7.4.py
@@ -1,12 +1,12 @@
 # 习题7.4
 import json
 import csv
-ls = []
 
-with open('price2016.csv') as csvfile:
-    content = csv.reader(csvfile)
-    for line in content:
-        ls.append(line)
+ls = []
+fr = open('price2016.csv')
+content = csv.reader(fr)
+for line in content:
+    ls.append(line)
 
 fw = open("price2016.json", "w")
 for i in range(1, len(ls)):
