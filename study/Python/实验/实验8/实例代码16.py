f8c9e9586137ef7d84ee0069ad86a1bdd12a642a
504703038 <504703038@github.com>
2019-05-30 21:03:00 +0000

    update

--- study/Python/实验/实验8/实例代码16.py
+++ study/Python/实验/实验8/实例代码16.py
@@ -1,14 +1,9 @@
 # e16.1BatchInstall.py
 import os
-libs = {
-    "numpy", "matplotlib", "pillow", "sklearn", "requests", "jieba",
-    "beautifulsoup4", "wheel", "networkx", "sympy", "pyinstaller", "django",
-    "flask", "werobot", "pyqt5", "pandas", "pyopengl", "pypdf2", "docopt",
-    "pygame"
-}
+libs = {"numpy", "matplotlib"}
 try:
     for lib in libs:
-        os.system("pip install " + lib)
+        os.system("pip3 install " + lib)
     print("Successful")
 except:
     print("Failed Somehow")
